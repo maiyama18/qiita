c2dd55d661009e825692e099da1f40a730005923
yam-net <myam0118@gmail.com>
2019-02-09 11:22:19 +0900

    reorder properties of Item

--- item.go
+++ item.go
@@ -9,15 +9,14 @@ type Item struct {
 	URL          string `json:"url"`
 	Body         string `json:"body"`
 	RenderedBody string `json:"rendered_body"`
+	Private      bool   `json:"private"`
+	Coediting    bool   `json:"coediting"`
 
 	CreatedAt time.Time `json:"created_at""`
 	UpdatedAt time.Time `json:"updated_at""`
 
-	Private        bool `json:"private"`
-	Coediting      bool `json:"coediting"`
-
-	PageViewsCount int  `json:"page_views_count"`
-	CommentsCount  int  `json:"comments_count"`
-	LikesCount     int  `json:"likes_count"`
-	ReactionsCount int  `json:"reactions_count"`
+	PageViewsCount int `json:"page_views_count"`
+	CommentsCount  int `json:"comments_count"`
+	LikesCount     int `json:"likes_count"`
+	ReactionsCount int `json:"reactions_count"`
 }
